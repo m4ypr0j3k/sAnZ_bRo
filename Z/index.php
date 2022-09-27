cbe2557ae309661d97891f01a927327019c4afc8
m4ypr0j3k <111507608+m4ypr0j3k@users.noreply.github.com>
2022-09-27 19:15:15 +0700

    Update index.php

--- Z/index.php
+++ Z/index.php
@@ -1,18 +1,17 @@
 <?php
-function keygen($length=10)
+
+function kataAcak()
 {
-	$key = '';
-	list($usec, $sec) = explode(' ', microtime());
-	mt_srand((float) $sec + ((float) $usec * 100000));
-   	$inputs = array_merge(range('z','a'),range(0,9),range('A','Z'));
-   	for($i=0; $i<$length; $i++)
-	{
-   	    $key .= $inputs{mt_rand(0,70)};
-	}
-	return $key;
-echo keygen(40)
- 
-// tambahkan 
-// echo keygen(40) untuk mendapatkan hasil
+    $kataacak = 
+    [
+        'acak 1',
+        'acak 2',
+        'acak 3'
+    ];
+
+    return $kataacak[mt_rand(0, count($kataacak) - 1)];
 }
+
+echo kataAcak();
+
 ?>
