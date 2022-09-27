791c9c1f2a4182e66dea8e9e6d79f46c346ef7a9
m4ypr0j3k <111507608+m4ypr0j3k@users.noreply.github.com>
2022-09-27 19:21:10 +0700

    Create random.php

---
+++ Z/random.php
@@ -0,0 +1,17 @@
+<?php
+function acak($panjang)
+{
+    $karakter= 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789';
+    $string = '';
+    for ($i = 0; $i < $panjang; $i++) {
+  $pos = rand(0, strlen($karakter)-1);
+  $string .= $karakter{$pos};
+    }
+    return $string;
+}
+//cara memanggilnya
+$hasil_1= acak(5);
+$hasil_2= acak(7);
+?>
+
+echo "{'kata satu':$hasil_1,'kata dua':$hasil_2}"
