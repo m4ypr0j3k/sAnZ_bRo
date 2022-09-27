0567d650b7ee76342b861a27d4276d819189ef11
m4ypr0j3k <111507608+m4ypr0j3k@users.noreply.github.com>
2022-09-27 19:07:51 +0700

    Create apikey.php

---
+++ apikey.php
@@ -0,0 +1,17 @@
+<?php
+function keygen($length=10)
+{
+	$key = '';
+	list($usec, $sec) = explode(' ', microtime());
+	mt_srand((float) $sec + ((float) $usec * 100000));
+   	$inputs = array_merge(range('z','a'),range(0,9),range('A','Z'));
+   	for($i=0; $i<$length; $i++)
+	{
+   	    $key .= $inputs{mt_rand(0,70)};
+	}
+	return $key;
+ 
+// tambahkan 
+// echo keygen(40) untuk mendapatkan hasil
+}
+?>
